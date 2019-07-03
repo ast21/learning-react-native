fbcce883059e4a18e1499c84fb76e1b8afcb6da1
Anastas Mironov <thebreakhack@gmail.com>
2019-07-04 00:20:41 +0900

    Redirect to employeeList

--- manager/src/Router.js
+++ manager/src/Router.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { Router, Scene, Stack } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
+import EmployeeList from './components/EmployeeList';
 
 const RouterComponent = () => {
   return (
     <Router>
-      <Stack key="root">
-        <Scene key="login" component={LoginForm} title="Please login" />
+      <Stack key="root" hideNavBar>
+
+        <Stack key="auth">
+          <Scene key="login" component={LoginForm} title="Please login" />
+        </Stack>
+
+        <Stack key="main">
+          <Scene key="employeeList" component={EmployeeList} title="Employees" />
+        </Stack>
+
       </Stack>
     </Router>
   )
